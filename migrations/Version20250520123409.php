01462cf1fdbebd2b3278179f922d5a0ddfea6c71
Niwa-Yume <julien.castro@protonmail.com>
2025-05-20 14:34:30 +0200

    ajout du status

---
+++ migrations/Version20250520123409.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250520123409 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE status (id INT AUTO_INCREMENT NOT NULL, status VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE event RENAME INDEX idx_event_created_by TO IDX_3BAE0AA7B03A8386
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            DROP TABLE status
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE event RENAME INDEX idx_3bae0aa7b03a8386 TO IDX_EVENT_CREATED_BY
+        SQL);
+    }
+}
