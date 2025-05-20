5ff10e1a691aec82e2062981b8a6883245f5b20c
Niwa-Yume <julien.castro@protonmail.com>
2025-05-20 15:21:25 +0200

    ajout du status de l'intéret

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Interet;
+use App\Entity\Status;
 use App\Form\NewEventForm;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,6 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Event;
 use App\controller;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 //ici on peut mettre un route pour définir les routes d'en dessous #[Route('/event', name: 'app_event_nom_de_la_route')]
 #[Route('/event', name: 'app_event_')]
@@ -32,16 +35,34 @@ final class EventController extends AbstractController
 
     // LE SHOW
     #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'])]
-    public function show(int $id, EventRepository $eventRepository): Response
-    {
+    public function show(
+        int $id,
+        EventRepository $eventRepository,
+        EntityManagerInterface $em
+    ): Response {
         $event = $eventRepository->find($id);
-
         if (!$event) {
             throw $this->createNotFoundException('Événement non trouvé.');
         }
 
+        // récupère tous les status
+        $statuses = $em->getRepository(Status::class)->findAll();
+
+        // récupère l'intérêt de l'utilisateur s'il est connecté
+        $interet = null;
+        if ($this->getUser()) {
+            $interet = $em
+                ->getRepository(Interet::class)
+                ->findOneBy([
+                    'event' => $event,
+                    'user'  => $this->getUser(),
+                ]);
+        }
+
         return $this->render('event/show.html.twig', [
-            'event' => $event,
+            'event'    => $event,
+            'statuses' => $statuses,
+            'interet'  => $interet,
         ]);
     }
 
@@ -106,5 +127,29 @@ final class EventController extends AbstractController
         }
 
         return $this->redirectToRoute('app_event_index');
+    }
+
+    //ROUTE POUR SIGNALER UN INTERET
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    #[Route('/{id}/interet', name: 'interet', methods: ['POST'])]
+    public function interet(Request $request, Event $event, EntityManagerInterface $em): Response
+    {
+        $user      = $this->getUser();
+        $statusId  = $request->request->get('status');
+        $status    = $em->getRepository(Status::class)->find($statusId);
+        $repo      = $em->getRepository(Interet::class);
+        $interet   = $repo->findOneBy(['event'=>$event, 'user'=>$user]) ?? new Interet();
+
+        $interet
+            ->setEvent($event)
+            ->setUser($user)
+            ->setStatus($status);
+        if (null === $interet->getId()) {
+            $interet->setCreatedAt(new \DateTimeImmutable());
+            $em->persist($interet);
+        }
+        $em->flush();
+
+        return $this->redirectToRoute('app_event_show', ['id'=>$event->getId()]);
     }
 }
